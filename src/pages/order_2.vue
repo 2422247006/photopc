e857a422b4fae1c7a6235057e7cc4053669e2e9c
unknown <2422247006>
2019-12-11 18:32:05 +0800

    18:32

--- src/pages/order_2.vue
+++ src/pages/order_2.vue
@@ -11,10 +11,10 @@
       <div class="headerdiv div8" @mouseover="descmouseover" @mouseleave="descmouseleave">
         <img src="../assets/icon/why.png" style="width:100%;height:100%;background:none" />
       </div>
-      <!-- <div style="margin-left:30%;">
-        <el-button type="primary">《前七天</el-button>
-        <el-button type="primary">后七天》</el-button>
-      </div> -->
+      <div style="margin-left:30%;">
+        <el-button type="primary" @click="beginclick()">《前七天</el-button>
+        <el-button type="primary" @click="endclick()">后七天》</el-button>
+      </div>
       <div class="desc" v-if="desc">
         <p>白色:表示可预约的时间点</p>
         <p style="statusor:gray">灰色:表示已过期的时间点</p>
@@ -28,24 +28,19 @@
     <div class="date">
       <div class="date_left"></div>
       <!-- :style="{width:index===11?'20px':'70px'}" -->
-      <div
-        
-        v-for="(item,index) of datelist"
-        :key="item"
-        class="dateitem"
-      >{{item}}</div>
+      <div v-for="(item,index) of datelist" :key="item" class="dateitem">{{item}}</div>
     </div>
     <div class="con" v-for="(item,index) of daylist" :key="item.id">
       <div class="left">
         <div class="dayl">
           <p>{{item.date}}</p>
-          <!-- <p>周六</p> -->
+          <p>{{item.week}}</p>
         </div>
         <!-- <div class="dayr">
           <p>可用总量:36</p>
           <p>可用余量:0</p>
           <p class="more" @mouseover="detailsmouseover(index)" @mouseleave="detailsmouseleave">详情</p>
-        </div> -->
+        </div>-->
         <div class="details" v-if="changeactive==index">
           <p class="p1">6月19日预约产品数量(包含未付款)</p>
           <p class="p2">总计：40</p>
@@ -55,20 +50,16 @@
           <p class="p3">证件照-正面-白色：7</p>
         </div>
       </div>
-      <div
-        class="time"
-        
-        v-for="(i,index) of item.timeList"
-      >
-      <!-- :style="{width:index===11?'20px':'70px'}" -->
-      <!-- width:index===11?'100%':'30%',background:s.status==='booked'?'black':s.status==='occupied'?'#1989fa':s.status==='finished'?'green':s.status==='closed'?'red':s.status==='Expired'?'#dddddd':s.status==='unpaid'?'orange':'white' -->
-         <div
-          class="bor"
-          :style="{background:s.status==='booked'?'black':s.status==='occupied'?'#1989fa':s.status==='finished'?'green':s.status==='closed'?'red':s.status==='Expired'?'#dddddd':s.status==='unpaid'?'orange':'white'}"
-          v-for="s of i.scheduleList"
-          :title="s.time"
-          @click="openclick(s.id,s.status)"
-        ></div>
+      <div class="time" v-for="(i,index) of item.timeList">
+        <!-- :style="{width:index===11?'20px':'70px'}" -->
+        <!-- width:index===11?'100%':'30%',background:s.status==='booked'?'black':s.status==='occupied'?'#1989fa':s.status==='finished'?'green':s.status==='closed'?'red':s.status==='Expired'?'#dddddd':s.status==='unpaid'?'orange':'white' -->
+        <div
+          class="bor"
+          :style="{background:s.status==='booked'?'black':s.status==='occupied'?'#1989fa':s.status==='finished'?'green':s.status==='closed'?'red':s.status==='Expired'?'#dddddd':s.status==='unpaid'?'orange':'white'}"
+          v-for="s of i.scheduleList"
+          :title="s.time"
+          @click="openclick(s.id,s.status)"
+        ></div>
       </div>
     </div>
   </div>
@@ -78,8 +69,10 @@
 export default {
   data() {
     return {
-      status_:'',
-      time_data:[],
+      off: 0,
+      datetime: "",
+      status_: "",
+      time_data: [],
       changeactive: -1,
       desc: false,
       details: false,
@@ -409,9 +402,40 @@ export default {
           ]
         }
       ]
-    }   
+    };
   },
   methods: {
+    //点击前七天
+    beginclick() {
+      var that = this;
+      var qqq = that.daylist.map(function(item) {
+        return item.date;
+      });
+      that.datetime = qqq[0];
+      // console.log(that.datetime)
+      // that.endtime = that.getBeforeDate(1, that.begintime);
+      // that.begintime = that.getBeforeDate(7, that.begintime);
+      // console.log(that.begintime + "前7开始哦");
+      // console.log(that.endtime + "前7结束哦");
+      // that.datetime = that.time_data[0];
+      that.off = -7;
+      that.gettimelist();
+    },
+    //点击后七天
+    endclick() {
+      var that = this;
+       var that = this;
+      var qqq = that.daylist.map(function(item) {
+        return item.date;
+      });
+      that.datetime = qqq[6];
+      // that.endtime = that.getafterDate(13, that.begintime);
+      // that.begintime = that.getafterDate(7, that.begintime);
+      // console.log(that.begintime + "后7开始哦");
+      // console.log(that.endtime + "后7结束哦");
+       that.off = 7;
+      that.gettimelist();
+    },
     descmouseover() {
       this.desc = true;
     },
@@ -424,44 +448,43 @@ export default {
     detailsmouseleave(index) {
       this.changeactive = -1;
     },
-    openclick(id,status) {
-       console.log(id,status)
-      if (status == 'closed') {
-        console.log(id)
-        this.status_='empty'
+    openclick(id, status) {
+      console.log(id, status);
+      if (status == "closed") {
+        console.log(id);
+        this.status_ = "empty";
         //红
         this.$confirm("该时间点已被人工关闭，不可对其操作", "已关闭的时间点", {
           confirmButtonText: "开放时间点",
           cancelButtonText: "取消"
         }).then(() => {
-           this.changestatus(id)
+          this.changestatus(id);
           this.$message({
             type: "success",
             message: "已开放!"
           });
         });
-      } else if (status == 'empty') {
-        this.status_='closed'
+      } else if (status == "empty") {
+        this.status_ = "closed";
         //白
         this.$confirm("该时间点暂无顾客预约", "空闲的时间点", {
           // confirmButtonText: "添加预约",
           cancelButtonText: "关闭时间点"
         })
           .then(() => {
-          
             this.$message({
               type: "success",
               message: "已添加!"
             });
           })
           .catch(() => {
-              this.changestatus(id)
+            this.changestatus(id);
             this.$message({
               type: "info",
               message: "已关闭"
             });
           });
-      } else if (status == 'booked') {
+      } else if (status == "booked") {
         //黑
         var aaa = "吴易凡";
         var ref = 'ref="print"';
@@ -481,75 +504,116 @@ export default {
           // });
         });
       }
-     
     },
     gettimelist() {
       var that = this;
+      // var qqq = String(that.begintime)
+      //     qqq = qqq.slice(0, 4) + "-" + qqq.slice(4);
+      //     qqq = qqq.slice(0, 7) + "-" + qqq.slice(7);
       that.$axios
         .get(that.$apiUrl + "/api/v1/schedule/query/range", {
           params: {
-            dateBegin: "20191210",
-            dateEnd: "20191216"
+            date: that.datetime,
+            off: that.off
           }
         })
         .then(function(res) {
-          // console.log(res.data.data);
-          that.daylist = res.data.data;
+          var aaa = res.data.data;
+          that.daylist = aaa.map(function(item) {
+            item.date = String(item.date);
+            item.date = item.date.slice(0, 4) + "-" + item.date.slice(4);
+            item.date = item.date.slice(0, 7) + "-" + item.date.slice(7);
+            return item;
+          });
         });
     },
-    changestatus(id){
+    changestatus(id) {
       var that = this;
       that.$axios
         .post(that.$apiUrl + "/api/v1/schedule/merge", {
-          id:id,
-          status:that.status_,
+          id: id,
+          status: that.status_
         })
         .then(function(res) {
-        
-           that.gettimelist();
+          that.gettimelist();
           // that.daylist = res.data.data;
         });
+    },
+    //求昨天-七天前
+    getBeforeDate(n, time) {
+      var n = n;
+      var d = new Date(time);
+      // console.log(d + "哈哈");
+      var year = d.getFullYear();
+      var mon = d.getMonth() + 1;
+      var day = d.getDate();
+      if (day <= n) {
+        if (mon > 1) {
+          mon = mon - 1;
+        } else {
+          year = year - 1;
+          mon = 12;
+        }
+      }
+      d.setDate(d.getDate() - n);
+      year = d.getFullYear();
+      mon = d.getMonth() + 1;
+      day = d.getDate();
+      var s =
+        year +
+        "-" +
+        (mon < 10 ? "0" + mon : mon) +
+        "-" +
+        (day < 10 ? "0" + day : day);
+      return s;
+    },
+    //求明天-七天后
+    getafterDate(n, time) {
+      var n = n;
+      var d = new Date(time);
+      // console.log(d + "哈哈");
+      var year = d.getFullYear();
+      var mon = d.getMonth() + 1;
+      var day = d.getDate();
+      if (day <= n) {
+        if (mon > 1) {
+          mon = mon + 1;
+        } else {
+          year = year + 1;
+          mon = 12;
+        }
+      }
+      d.setDate(d.getDate() + n);
+      year = d.getFullYear();
+      mon = d.getMonth() + 1;
+      day = d.getDate();
+      var s =
+        year +
+        "-" +
+        (mon < 10 ? "0" + mon : mon) +
+        "-" +
+        (day < 10 ? "0" + day : day);
+      return s;
     }
   },
   created() {
-    this.gettimelist();
-    // const temp = [];
+    //初始获取今天至未来七天
     const onyear = [];
-  
-    // var weeklist = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"];
     for (let i = 0; i < 7; i++) {
       const time = new Date(new Date().setDate(new Date().getDate() + i));
       const year = `${time.getFullYear()}`;
       const month = `0${time.getMonth() + 1}`.slice(-2);
       const week = `${time.getDay()}`.slice(-2);
-      // const weekday = weeklist[week];
       const strDate = `0${time.getDate()}`.slice(-2);
-      // temp.push(`${weekday}${month}月${strDate}日`);
-      onyear.push(`${year}${month}${strDate}`);
-      // onyear_.push(`${year}-${month}-${strDate}`);
+      onyear.push(`${year}-${month}-${strDate}`);
     }
-    console.log(onyear);
-    this.time_data = onyear
-    // this.time_data = temp.map(function(item, index) {
-    //   return { id: index, date: item };
-    // });
-
-    // this.time_data = this.time_data.map((v, i) => {
-    //   v.format = onyear[i];
-    //   return v;
-    // });
-    // this.time_data = this.time_data.map((v, i) => {
-    //   v.format_ = onyear_[i];
-    //   return v;
-    // });
-    console.log(this.time_data);
-  //   var aaa=this.time_data.map(function(item){
-  // return item.format
-  //   })
-  //   console.log(aaa)
-  //  aaa.splice(1,5)
-  //    console.log(aaa)
-  }
+    this.time_data = onyear;
+    this.time_data.splice(1, 5);
+    this.datetime = this.time_data[0];
+    // this.endtime = this.time_data[1];
+    this.gettimelist();
+  },
+  mounted() {}
 };
 </script>
 <style scoped>
@@ -637,6 +701,7 @@ export default {
   display: flex;
   border: 1px solid rgb(221, 221, 221);
   position: relative;
+  border-radius: 5px;
 }
 .details {
   width: 250px;
